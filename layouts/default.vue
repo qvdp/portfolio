4ae8523b789cce333fa87ff9001b45adb6bfc0ab
qvdp <vanderperre.quentin@gmail.com>
2020-07-13 19:53:39 +0200

    initial: first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-100 dark:bg-black">
+    <lighting-switcher />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import LightingSwitcher from '@/components/template/LightingSwitcher'
+
+export default {
+  components: {
+    LightingSwitcher
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center text-center mx-auto">
+    <div class="text-gray-700 dark:text-white">
+      <Logo />
+      <h1 class="title">
+        portfolio
+      </h1>
+    </div>
+  </div>
+</template>
