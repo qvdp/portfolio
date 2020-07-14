401ccf5c27f4a31848dd74c856853b9cc9ee2062
qvdp <vanderperre.quentin@gmail.com>
2020-07-14 15:33:17 +0200

    🚀 add conten

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
   <full-page :options="{ licenseKey: '98DCED20-01AA410B-98E959B2-BED07DE8' }">
-    <div class="section">
+    <div class="section text-gray-600 dark:text-white">
       <lighting-switcher />
+      <p>
+        First section ...
+      </p>
     </div>
-    <div class="section">
-      Second section ...
+    <div class="section text-gray-600 dark:text-white">
+      <p>
+        Second section ...
+      </p>
     </div>
   </full-page>
 </template>
